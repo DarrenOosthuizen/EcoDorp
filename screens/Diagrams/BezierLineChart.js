31fdaf7a1447d149611aacce337f21f854b65279
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-05-27 14:05:51 +0200

    Added BezierLineChart Component

---
+++ screens/Diagrams/BezierLineChart.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { View, Text, Button, StyleSheet, Dimensions } from "react-native";
+import { LineChart } from "react-native-chart-kit";
+const BezierLineChart = (props) => {
+  const label = [];
+  props.labelheading.forEach((element) => {
+    label.push(element);
+  });
+
+  const datas = [];
+  props.datavalue.forEach((element) => {
+    datas.push(element);
+  });
+
+  return (
+    <View>
+      <Text>Bezier Line Chart View</Text>
+      <LineChart
+        data={{
+          labels: label,
+          datasets: [
+            {
+              data: datas,
+            },
+          ],
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+    </View>
+  );
+};
+
+export default BezierLineChart;
