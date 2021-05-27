4335cd7f1f3028ab70377d76289faf5f4aeb5757
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-05-27 14:07:10 +0200

    Added Charts to Home Page component

--- screens/MainScreens/HomeScreen.js
+++ screens/MainScreens/HomeScreen.js
@@ -1,11 +1,95 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
+import BezierLineChart from "../Diagrams/BezierLineChart";
+import LineChart from "../Diagrams/LineChart";
+import BarChart from "../Diagrams/BarChart";
+import PieChart from "../Diagrams/PieChart";
+
+
+
 
 const HomeScreen = () => {
+
+  const testobject = [
+    {
+      name: "Random Info",
+      population: 102120120,
+      color: "rgba(220,220,120,1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "This is why wdwdwadawdawd",
+      population: 102120120,
+      color: "rgba(180,10,120,1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Helo There",
+      population: 102120120,
+      color: "rgba(220,120,120,1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Corsair",
+      population: 102120120,
+      color: "rgba(120,120,320,1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container} id="test">
+        <BezierLineChart style={styles.test}
+          labelheading={[
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            
+          ]}
+          datavalue={[10, 20, 15, 20, 30, 50, 20, 30]}
+        />
+        <LineChart
+          labelheading={[
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+          ]}
+          datavalue={[10, 20, 15, 20, 30, 50, 20, 30]}
+        />
+        <BarChart
+          labelheading={[
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+          ]}
+          datavalue={[0.2, 0.6, 0.5, 0.7, 0.2]}
+        />
+        <PieChart pieobject={testobject} />
+        <PieChart pieobject={testobject} />
+        <PieChart pieobject={testobject} />
+        <PieChart pieobject={testobject} />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -16,5 +100,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    marginLeft : '5%',
+    marginRight : '5%',
   },
 });
