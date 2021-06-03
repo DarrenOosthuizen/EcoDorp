72fdb0e3e2d35e3f91a2e9f63872410cb65a5878
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-03 13:19:48 +0200

    added data and components

--- screens/MainScreens/PredictionScreen.js
+++ screens/MainScreens/PredictionScreen.js
@@ -1,11 +1,57 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
+import NormalPrediction from "./Predictions/NormalPrediction";
 
-const PredictionScreen = () => {
+const PredictionScreen = (props) => {
+  var values = [{}];
+
+  const setValue = () => {
+    values = [
+      {
+        key: 1,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
+        heading: "Heading 1",
+      },
+      {
+        key: 2,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
+        heading: "Heading 2",
+      },
+      {
+        key: 3,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
+        heading: "Heading 3",
+      },
+      {
+        key: 4,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
+        heading: "Heading 4",
+      },
+      {
+        key: 5,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
+        heading: "Heading 5",
+      },
+      {
+        key: 6,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
+        heading: "Heading 6",
+      },
+    ];
+  };
+  setValue();
   return (
-    <View style={styles.container}>
-      <Text>Predictions Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {values.map((prediction) => (
+          <NormalPrediction
+            key={prediction.key}
+            text={prediction.text}
+            heading={prediction.heading}
+          />
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
@@ -15,6 +61,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    //justifyContent: "center",
+  },
+  items: {
+    backgroundColor: "rgb(240,201,201)",
   },
 });
