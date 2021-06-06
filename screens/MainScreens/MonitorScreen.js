7af3c967be018bba9b10403c5715a0d98347a18f
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-06 23:28:12 +0200

    Added Reading Component and Test Data

--- screens/MainScreens/MonitorScreen.js
+++ screens/MainScreens/MonitorScreen.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 import DropDownPicker from 'react-native-dropdown-picker';
+import Readings from './Readings/Reading';
 
 const MonitorScreen = () => {
+  let object = {
+    "date": "2021-06-06T20:14:49.846Z",
+    "temp": 20.2,
+    "hum": 42.3,
+    "pm": 3,
+    "tvoc": 40,
+    "co2":503,
+    "co": 0,
+    "air": 1017.2,
+    "ozone": 11,
+    "no2": 89,
+    "virus": 2,
+  }
   return (
     <View style={styles.container}>
-      <Text>Monitor Screen</Text>
+      <Readings {...object}/>
     </View>
   );
 };
