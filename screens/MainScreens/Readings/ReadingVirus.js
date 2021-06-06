68a4f1a3d58deea08ce49a1d3178a72dc4a3a6b1
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-07 00:02:16 +0200

    Created Virus Indicator

---
+++ screens/MainScreens/Readings/ReadingVirus.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const ReadingVirus = (props) => {
+  const GetVirus = (virus) =>
+  {
+    if(virus<=4)
+    {
+      return "GOOD"
+    }
+    if(virus>4)
+    {
+      return "BAD"
+    }
+  }
+  const GetColor = (virus) =>
+  {
+    if(virus<=4)
+    {
+      return "#51D257"
+    }
+    if(virus>4)
+    {
+      return "#EF1D10"
+    }
+  }
+  return (
+    <View style={{backgroundColor: GetColor(props.value),
+    width: "80%",
+    borderRadius: 10,
+    justifyContent: 'center'
+    }}>
+            <Text style={styles.text}>{GetVirus(props.value)}</Text>
+    </View>
+  );
+};
+
+export default ReadingVirus;
+
+const styles = StyleSheet.create({
+
+  text:
+  {
+    color : '#fff',
+    fontWeight: 'bold',
+    fontSize: 15,
+    textAlign: 'center'
+  }
+});
