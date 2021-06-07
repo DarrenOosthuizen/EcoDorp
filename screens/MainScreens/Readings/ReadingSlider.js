56662ad8e393a32fcba7535e697420b28368a032
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-07 22:42:16 +0200

    Updating Slider to auto Change

--- screens/MainScreens/Readings/ReadingSlider.js
+++ screens/MainScreens/Readings/ReadingSlider.js
@@ -3,17 +3,20 @@ import { View, Text, Button, StyleSheet } from "react-native";
 
 const ReadingSlider = (props) => {
   return (
-    <View style={styles.container}>
-        <View style={{color : '#ffff',
-      backgroundColor : "#fff",
-      width : 8,
-      height: 8,
-      borderRadius: 10,
-      justifyContent: 'center',
-      left: props.value + '%',
-      }}>
-            <Text></Text>
-        </View>
+    <View style={[{ backgroundColor: props.color }, styles.container]}>
+      <View
+        style={{
+          color: "#ffff",
+          backgroundColor: "#fff",
+          width: 8,
+          height: 8,
+          borderRadius: 10,
+          justifyContent: "center",
+          left: props.value + "%",
+        }}
+      >
+        <Text></Text>
+      </View>
     </View>
   );
 };
@@ -22,10 +25,9 @@ export default ReadingSlider;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#51D257",
     width: "100%",
     borderRadius: 10,
-    height: '40%',
-    justifyContent: 'center'
+    height: "40%",
+    justifyContent: "center",
   },
 });
