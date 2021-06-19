959074b19f1d4e64049e72023e5c9edefc8b0cac
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-20 00:19:09 +0200

    Sensor Data Component

---
+++ screens/MainScreens/SensorData/SensorData/SensorData.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  useWindowDimensions,
+  ActivityIndicator,
+  ScrollView,
+} from "react-native";
+
+import Humidity from "./Hum";
+import Temperature from "./Temp";
+import AirPressure from "./Air";
+import NO2 from "./NO2";
+import CO2 from "./CO2";
+import Ozone from "./Ozone";
+import PM from "./Pm";
+import TVOC from "./Tvoc";
+
+const SensorData = (props) => {
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {/* Displaying Temperature */}
+        <Temperature sensorID={props.sensorID} dataID={props.dataID} />
+
+        {/* Displaying Humidity */}
+        <Humidity sensorID={props.sensorID} dataID={props.dataID} />
+
+        {/* Displaying Air Pressure */}
+        <AirPressure sensorID={props.sensorID} dataID={props.dataID} />
+
+        {/* Displaying CO2 */}
+        <CO2 sensorID={props.sensorID} dataID={props.dataID} />
+
+        {/* Displaying NO2 */}
+        <NO2 sensorID={props.sensorID} dataID={props.dataID} />
+
+        {/* Displaying Ozone */}
+        <Ozone sensorID={props.sensorID} dataID={props.dataID} />
+
+        {/* Displaying PM */}
+        <PM sensorID={props.sensorID} dataID={props.dataID} />
+
+        {/* Displaying TVOC */}
+        <TVOC sensorID={props.sensorID} dataID={props.dataID} />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default SensorData;
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+  },
+});
