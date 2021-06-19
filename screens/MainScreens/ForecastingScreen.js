c27912cf837066f927cefb72823188af86f9233a
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-19 15:14:47 +0200

    Removed ForeCasting

--- screens/MainScreens/ForecastingScreen.js
+++ screens/MainScreens/ForecastingScreen.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { View, Text, Button, StyleSheet,useWindowDimensions  } from "react-native";
 import BottomTab from './ForeCasting/BottomNavigator';
-import ForeCast from "./ForeCasting/ForeCastScreen";
+
 
 
 const Forecasting = () => {
   return (
     <View style={styles.container}>
       <BottomTab/>
-      {/* <ForeCast forecast={"temperature"}/> */}
+
     </View>
   );
 };
