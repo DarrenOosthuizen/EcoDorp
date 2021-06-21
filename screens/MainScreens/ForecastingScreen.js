93c87506062d610c2366d554766ac6ed5d3000d0
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-21 15:54:45 +0200

    Added view for if user dont have any sensors

--- screens/MainScreens/ForecastingScreen.js
+++ screens/MainScreens/ForecastingScreen.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet,useWindowDimensions  } from "react-native";
 import BottomTab from './ForeCasting/BottomNavigator';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Host } from "../env";
 
 
 
 const Forecasting = () => {
-  return (
-    <View style={styles.container}>
-      <BottomTab/>
+  const [shouldRender, setShouldRender] = useState(false);
+  useEffect(() => {
+    getUserData();
+  }, []);
 
-    </View>
-  );
+  async function getUserData() {
+    try {
+      userToken = await AsyncStorage.getItem("userToken");
+    } catch (e) {}
+
+    let result = await fetch(Host + "/sensors", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: userToken,
+      },
+    });
+    result = await result.json();
+    if (result.length == 0) {
+      setShouldRender(false);
+    } else {
+      setShouldRender(true);
+    }
+  }
+
+  function RenderDisplay() {
+    if (shouldRender == false) {
+      return (
+        <View style={styles.cont}>
+          <Text style={styles.texthead}>No sensors linked to User</Text>
+          <Text style={styles.textpar}>Please add Sensor under Manage Devices Tab</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <BottomTab />
+        </View>
+      );
+    }
+  }
+
+  return <View>{RenderDisplay()}</View>;
 };
 
 export default Forecasting;
@@ -19,5 +59,17 @@ const styles = StyleSheet.create({
   container: {
     height: '100%',
     width: '100%'
+  },
+  cont: {
+    height: "100%",
+    width: "100%",
+    justifyContent: 'center',
+    alignItems : 'center'
+  },
+  texthead : {
+    fontSize : 28
+  },
+  textpar : {
+    fontSize : 18
   },
 });
