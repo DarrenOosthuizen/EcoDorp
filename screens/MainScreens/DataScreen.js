d6b96fc941439f3dc5bf884dffc71cd98752a8f7
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-21 15:22:19 +0200

    Implemented Different view for if user has no components on their account

--- screens/MainScreens/DataScreen.js
+++ screens/MainScreens/DataScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -9,12 +9,56 @@ import {
   StatusBar,
 } from "react-native";
 import BottomTab from "./SensorData/BottomNavigatorData";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Host } from "../env";
 
 const DataScreen = () => {
-  return (
-    <View style={styles.container}>
+  const [shouldRender, setShouldRender] = useState(false);
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  async function getUserData() {
+    try {
+      userToken = await AsyncStorage.getItem("userToken");
+    } catch (e) {}
+
+    let result = await fetch(Host + "/sensors", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: userToken,
+      },
+    });
+    result = await result.json();
+    if (result.length == 0) {
+      setShouldRender(false);
+    } else {
+      setShouldRender(true);
+    }
+  }
+
+  function RenderDisplay() {
+    if (shouldRender == false) {
+      return (
+        <View style={styles.cont}>
+          <Text style={styles.texthead}>No sensors linked to User</Text>
+          <Text style={styles.textpar}>Please add Sensor under Manage Devices Tab</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
       <BottomTab />
     </View>
+      );
+    }
+  }
+
+
+  return (
+    <View>{RenderDisplay()}</View>
   );
 };
 
@@ -24,5 +68,17 @@ const styles = StyleSheet.create({
   container: {
     height: "100%",
     width: "100%",
+  },
+  cont: {
+    height: "100%",
+    width: "100%",
+    justifyContent: 'center',
+    alignItems : 'center'
+  },
+  texthead : {
+    fontSize : 28
+  },
+  textpar : {
+    fontSize : 18
   },
 });
