343f05b175f3d4f9c5cc4bab086060a77656808c
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-21 15:24:17 +0200

    Added view to show if user does not have any sensors on their account

--- screens/MainScreens/MonitorScreen.js
+++ screens/MainScreens/MonitorScreen.js
@@ -1,13 +1,54 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
 import BottomNav from "./Monitor/BottomNavigator";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Host } from "../env";
 
 const MonitorScreen = (props) => {
-  return (
-    <View style={styles.container}>
-      <BottomNav />
-    </View>
-  );
+  const [shouldRender, setShouldRender] = useState(false);
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  async function getUserData() {
+    try {
+      userToken = await AsyncStorage.getItem("userToken");
+    } catch (e) {}
+
+    let result = await fetch(Host + "/sensors", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: userToken,
+      },
+    });
+    result = await result.json();
+    if (result.length == 0) {
+      setShouldRender(false);
+    } else {
+      setShouldRender(true);
+    }
+  }
+
+  function RenderDisplay() {
+    if (shouldRender == false) {
+      return (
+        <View style={styles.cont}>
+          <Text style={styles.texthead}>No sensors linked to User</Text>
+          <Text style={styles.textpar}>Please add Sensor under Manage Devices Tab</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <BottomNav />
+        </View>
+      );
+    }
+  }
+
+  return <View>{RenderDisplay()}</View>;
 };
 
 export default MonitorScreen;
@@ -16,5 +57,17 @@ const styles = StyleSheet.create({
   container: {
     height: "100%",
     width: "100%",
+  },
+  cont: {
+    height: "100%",
+    width: "100%",
+    justifyContent: 'center',
+    alignItems : 'center'
+  },
+  texthead : {
+    fontSize : 28
+  },
+  textpar : {
+    fontSize : 18
   },
 });
