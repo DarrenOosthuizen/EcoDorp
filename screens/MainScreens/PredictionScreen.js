f0e7921d2e92906d9b607de9f22091070781a386
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-21 15:25:03 +0200

    Updated which component is shown on preditions

--- screens/MainScreens/PredictionScreen.js
+++ screens/MainScreens/PredictionScreen.js
@@ -1,57 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
-import NormalPrediction from "./Predictions/NormalPrediction";
+import BottonNav from "./Predictions/BottomNavigator";
 
 const PredictionScreen = (props) => {
-  var values = [{}];
-
-  const setValue = () => {
-    values = [
-      {
-        key: 1,
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
-        heading: "Heading 1",
-      },
-      {
-        key: 2,
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
-        heading: "Heading 2",
-      },
-      {
-        key: 3,
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
-        heading: "Heading 3",
-      },
-      {
-        key: 4,
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
-        heading: "Heading 4",
-      },
-      {
-        key: 5,
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
-        heading: "Heading 5",
-      },
-      {
-        key: 6,
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat et lectus porttitor faucibus. Praesent nec ante ante. Nunc sed dui at justo pretium tincidunt eu non mi. Nunc.",
-        heading: "Heading 6",
-      },
-    ];
-  };
-  setValue();
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        {values.map((prediction) => (
-          <NormalPrediction
-            key={prediction.key}
-            text={prediction.text}
-            heading={prediction.heading}
-          />
-        ))}
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <BottonNav />
+    </View>
   );
 };
 
@@ -59,11 +14,8 @@ export default PredictionScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    //justifyContent: "center",
+    width : '100%',
+    height: '100%'
   },
-  items: {
-    backgroundColor: "rgb(240,201,201)",
-  },
+
 });
